31c9865ee6243f7f395e1f77ad9a1a5e0e16969c
shahfarazz <shahfarazulhaq@gmail.com>
2022-09-30 18:23:06 +0500

    3 getters 3 setters contract

--- aerospace_contract.sol
+++ aerospace_contract.sol
@@ -11,20 +11,38 @@ contract AerospaceContract{
    // State variables are variables whose values are permanently stored in contract storage. 
    // The keyword `public` makes variables accessible from outside a contract and creates a 
    // function that other contracts or clients can call to access the value.
-   string public aircraft_manufacture_location;
-   string public assembly_date;
-   string public technician_name;
+   string private aircraft_manufacture_location;
+   string private assembly_date;
+   string private technician_name;
 
    // Similar to many class-based object-oriented languages, a constructor is a special function that is only executed upon contract creation.
    // Constructors are used to initialize the contract's data. Learn more:https://solidity.readthedocs.io/en/v0.5.10/contracts.html#constructors
    constructor() {
    }
 
-   // A public function that accepts a string argument and updates the `message` storage variable.
-   function addValues(string memory aircraft_loc,string memory assembl_date,string memory tech_name) public {
-      aircraft_manufacture_location = aircraft_loc;
+   // A public function that accepts a string argument and updates the `aircraftlocation` storage variable.
+   function setLocation(string memory aircraft_loc) public {
+      aircraft_manufacture_location = aircraft_loc; 
+   }
+
+   function setDate(string memory assembl_date) public {
       assembly_date = assembl_date;
-      technician_name = tech_name; 
+   }
+
+   function setTechName(string memory tech_name) public {
+      technician_name = tech_name;
+   }
+
+   function getLocation() public view returns (string memory){
+       return aircraft_manufacture_location;
+   }
+
+   function getTechName() public view returns (string memory){
+       return technician_name;
+   }
+
+   function getDate() public view returns (string memory){
+       return assembly_date;
    }
 
 }
